8eeedac71be5908af1c5341726f8431bf319d765
Arshpreet Singh <arshsomal100@gmail.com>
2024-10-22 18:51:24 +0530

    fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,11 @@ connectDatabase();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-    origin : 'http://localhost:5173',
-    credentials : true,
-}
+    origin: 'http://localhost:5173', 
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
