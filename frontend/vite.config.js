5488fc8a32fc434c3be3ac2fe45b83aa71fb9bbe
Arshpreet Singh <arshsomal100@gmail.com>
2024-11-30 11:48:16 +0530

    Added vercel.json file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,4 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx', // Ensure this is pointing to your frontend entry file
-    },
-  }
 })
