56971f3a1b2b1690e72a698df5e6d0a6f1aae0cd
Arshpreet Singh <arshsomal100@gmail.com>
2024-11-30 12:13:25 +0530

    Added vercel.json file

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ connectDatabase();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174'], 
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
